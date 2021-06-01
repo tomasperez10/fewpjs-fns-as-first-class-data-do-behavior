d271554f5dff8ac2bb4f1971d59d8b42eb120c1f
Tomas Perez <tomasitopg6@gmail.com>
2021-06-01 09:17:19 +0000

    done

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(timeString) {
+  const time = timeString.split(":")
+  const hour = parseInt(time[0])
+
+  if (hour < 12) {
+    return "Good Morning"
+  } else if (hour >= 12 && hour <= 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(message) {
+  const header = document.getElementById('greeting')
+
+  header.innerText = message;
+}
